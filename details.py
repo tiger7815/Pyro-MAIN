d216fbdc39a7196d366b6ba426634477b6135f3e
tiger7815 <141488144+tiger7815@users.noreply.github.com>
2023-08-28 02:50:10 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 22539447
 api_hash = "de7fc74fdcad8cb0a621fffcc346fba4"
-bot_token = "6394660232:AAGvD3AKt2vZ364XcOVOb59YZgk0wDoH8d4"
+bot_token = "6367203217:AAEicdMBX_4SW7b8hEmCjDNfaMX2EuCn0UQ"
 
